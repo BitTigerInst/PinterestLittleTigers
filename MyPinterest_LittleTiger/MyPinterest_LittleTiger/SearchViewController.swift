b71afb6026baea7e5804dcd1bb13a511cc7b0084
rovero <wuyaohong@cony.local>
2016-08-08 11:32:01 +0800

    update files

--- MyPinterest_LittleTiger/MyPinterest_LittleTiger/SearchViewController.swift
+++ MyPinterest_LittleTiger/MyPinterest_LittleTiger/SearchViewController.swift
@@ -1,25 +1,26 @@
 //
-//  SecondViewController.swift
+//  SearchViewController.swift
 //  MyPinterest_LittleTiger
 //
-//  Created by Sijie Liang on 8/2/16.
-//  Copyright © 2016 Carnegie Mellon University. All rights reserved.
+//  Created by 吴耀洪 on 16/8/8.
+//  Copyright © 2016年 Carnegie Mellon University. All rights reserved.
 //
+
 
 import UIKit
 
-class SecondViewController: UIViewController {
-
+class SearchViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
---
+++ MyPinterest_LittleTiger/MyPinterest_LittleTiger/SettingsViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  MyPinterest_LittleTiger
+//
+//  Created by Sijie Liang on 8/2/16.
+//  Copyright © 2016 Carnegie Mellon University. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
