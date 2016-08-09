ad948c261961cb43d926741a81117dc91dbaec9b
rovero <wuyaohong@cony.local>
2016-08-09 20:20:00 +0800

    update

---
+++ MyPinterest_LittleTiger/MyPinterest_LittleTiger/AccountViewController.swift
@@ -0,0 +1,27 @@
+//
+//  AccountViewController.swift
+//  MyPinterest_LittleTiger
+//
+//  Created by 吴耀洪 on 16/8/9.
+//  Copyright © 2016年 Carnegie Mellon University. All rights reserved.
+//
+
+import UIKit
+
+class AccountViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    @IBAction func backClicked(sender: AnyObject) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+}
