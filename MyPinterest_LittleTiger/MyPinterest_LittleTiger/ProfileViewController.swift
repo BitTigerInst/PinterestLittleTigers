dcbb93b4239a06d860d3ef14f95ee58b0bf87d8a
rovero <wuyaohong@cony.local>
2016-08-14 13:54:38 +0800

    update

---
+++ MyPinterest_LittleTiger/MyPinterest_LittleTiger/ProfileViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ProfileViewController.swift
+//  MyPinterest_LittleTiger
+//
+//  Created by 吴耀洪 on 16/8/14.
+//  Copyright © 2016年 Carnegie Mellon University. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func backClicked(sender: AnyObject) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+}
