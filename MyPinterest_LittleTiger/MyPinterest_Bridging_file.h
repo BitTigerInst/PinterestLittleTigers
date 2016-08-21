649bfe7c3ecc1ae34df708bb7f8be1dc70940600
Marathon-fan <zhouqf2013@163.com>
2016-08-22 00:05:42 +0800

    Add FlickrKit. Calling OC method through swift.

---
+++ MyPinterest_LittleTiger/MyPinterest_Bridging_file.h
@@ -0,0 +1,15 @@
+//
+//  MyPinterest_Bridging_file.h
+//  MyPinterest_LittleTiger
+//
+//  Created by M'K on 8/21/16.
+//  Copyright © 2016 Carnegie Mellon University. All rights reserved.
+//
+
+#ifndef MyPinterest_Bridging_file_h
+#define MyPinterest_Bridging_file_h
+
+import "FlickrKitFramework.h"
+
+
+#endif /* MyPinterest_Bridging_file_h */
--- MyPinterest_LittleTiger/MyPinterest_LittleTiger/SearchViewController.swift
+++ MyPinterest_LittleTiger/MyPinterest_LittleTiger/SearchViewController.swift
@@ -9,6 +9,8 @@
 
 
 import UIKit
+import FlickrKitFramework
+
 
 class SearchViewController: UIViewController {
     
