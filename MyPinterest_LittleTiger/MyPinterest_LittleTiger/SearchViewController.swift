9550fbee1aa742db05f240bc9f269c937fa6639a
Marathon-fan <zhouqf2013@163.com>
2016-08-23 07:39:52 +0800

    add FlickrKit and some modifications.

--- MyPinterest_LittleTiger/MyPinterest_LittleTiger/SearchViewController.swift
+++ MyPinterest_LittleTiger/MyPinterest_LittleTiger/SearchViewController.swift
@@ -7,15 +7,18 @@
 //  Copyright © 2016年 Carnegie Mellon University. All rights reserved.
 //
 
-
 import UIKit
 import FlickrKitFramework
 
-
 class SearchViewController: UIViewController {
+    
+    var _flickrKitModel:FlickrKit = FlickrKit.sharedFlickrKit()//singleton
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+         _flickrKitModel.initializeWithAPIKey("22a884d55650bb34ca04ddc843d8a0e3", sharedSecret: "2cc44921f60ec846")
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
     
